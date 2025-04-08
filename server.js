207603360243d62fb40acb41c1001409a427406c
MuhammadUsman9876 <musman.shaikh98@gmail.com>
2025-04-09 04:07:21 +0500

    Your commit message

--- server.js
+++ server.js
@@ -6,12 +6,25 @@ const app = express();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 5000;  // Fallback to 5000 if PORT isn't defined
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+}).on('error', (err) => {
+  console.error('Server Error:', err);
+});
+
+
 // Enable CORS for all routes
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',  // Local development
+  'https://reactproject-2025-production.up.railway.app'  // Production URL
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+}));
 
 // Add /api prefix to the routes
 app.use('/api', router); // This will now serve /api/jobs, /api/posts, etc.
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
-});
+
